eb9bb07634d70fb752a978282c7b125a8897bb06
omar hosni <omarhosni433@gmail.com>
2026-06-02 12:55:24 +0300

    fix vercel config remove api rewrite

--- src/app/features/allorders/service/allorders.service.ts
+++ src/app/features/allorders/service/allorders.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../../../environments/environment.development';
+import { environment } from '../../../../environments/environment';
 import { jwtDecode } from "jwt-decode";
 import { CookieService } from 'ngx-cookie-service';
 import { DecodeToken } from '../../../core/auth/models/decode-token.interface';
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  baseUrl:"https://ecommerce.routemisr.com/api/v1/"
+  baseUrl:"https://ecommerce.routemisr.com/api/v1/",
+  production: false,
+  paymentRedirectUrl: 'http://localhost:4200/#/allorders'
 };
